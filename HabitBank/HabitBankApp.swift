d11915b5a7d3519196ecc5b75982a7e7bbb372a2
michael-h-roberson <michael.h.roberson@gmail.com>
2024-05-29 10:14:28 -0400

    Initial Commit

---
+++ HabitBank/HabitBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBankApp.swift
+//  HabitBank
+//
+//  Created by Michael Roberson on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
